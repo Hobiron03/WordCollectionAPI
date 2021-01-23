8fed67a5769f029e899269e3f2bad6b91d875931
HOBIRON <yudainepnep@icloud.com>
2021-01-23 20:45:48 +0900

    feat: GetUSerWordAll() in words.go

--- app/models/words.go
+++ app/models/words.go
@@ -1,6 +1,9 @@
 package models
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 type Word struct {
 	ID        int
@@ -9,16 +12,33 @@ type Word struct {
 	Mean      string
 	Pronounce string
 	Genre     string
-	color     string
+	Color     string
 }
 
 func (u *User) CreateWord(word string, mean string, pronounce string, genre string, color string) (err error) {
-	cmd := `insert into words (user_id, word, mean, pronounce, genre, color, created_at) values (?, ?, ?, ?, ?, ?)`
+	cmd := `insert into words (user_id, word, mean, pronounce, genre, color, created_at) values (?, ?, ?, ?, ?, ?, ?)`
 
-	_, err = Db.Exec(cmd, u.ID, word, mean, pronounce, genre, color)
+	_, err = Db.Exec(cmd, u.ID, word, mean, pronounce, genre, color, time.Now())
 	if err != nil {
 		log.Fatalln(err)
 	}
 
 	return err
+}
+
+func (u *User) GetUserWordAll() (words []Word, err error) {
+	cmd := `select id, user_id, word, mean, pronounce, genre, color from words where user_id = ?`
+	rows, err := Db.Query(cmd, u.ID)
+
+	for rows.Next() {
+		var word Word
+		err = rows.Scan(&word.ID, &word.UserID, &word.Word, &word.Mean, &word.Pronounce, &word.Genre, &word.Color)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		words = append(words, word)
+	}
+	rows.Close()
+
+	return words, err
 }
